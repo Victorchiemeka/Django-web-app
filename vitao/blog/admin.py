fc9e5bc2e829dde5fbaf28187d11317ebd795456
victorchiemeka <victorchukwuemeka127@gmail.com>
2024-01-12 05:47:02 -0800

    db migration

--- vitao/blog/admin.py
+++ vitao/blog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import POST
 
-# Register your models here.
+admin.site.register(POST)
--- vitao/blog/models.py
+++ vitao/blog/models.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class POST(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)  # max_length is required
+    content = models.TextField()  # trd: text field
+    date_posted = models.DateTimeField(
+        default=timezone.now
+    )  # timezone.now is a function, not a function call
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # on_delete: if user is deleted, delete the post as well
+
+    def __str__(self):
+        return self.title
